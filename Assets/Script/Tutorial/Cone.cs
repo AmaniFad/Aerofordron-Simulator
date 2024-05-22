cfa95acb4110e542ba25e439ad36c661facaa3aa
AmaniFad <a22amafaddok@inspedralbes.cat>
2024-05-22 10:03:12 +0200

    mas cosas

--- Assets/Script/Tutorial/Cone.cs
+++ Assets/Script/Tutorial/Cone.cs
@@ -5,7 +5,13 @@ using UnityEngine.Events;
 
 public class Cone : MonoBehaviour
 {
-    [SerializeField] private UnityEvent isTigger;
+    [SerializeField] private UnityEvent OnFirstPart;
+    [SerializeField] private UnityEvent OnSecondPart;
+    [SerializeField] private GameObject panel1; 
+    [SerializeField] private GameObject panel2;
+
+    private bool isfirstPart;
+    private bool isSecondPart;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +28,29 @@ public class Cone : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            isTigger.Invoke();
+            if (!isfirstPart)
+            {
+                OnFirstPart.Invoke();
+                isfirstPart = true;
+            }
+            else if (!isSecondPart)
+            {
+                OnSecondPart.Invoke();
+                isSecondPart = true;    
+            }
         }
+    }
+
+    public void changePanel()
+    {
+        panel1.SetActive(true);
+        StartCoroutine(_ChangeCanvas());
+    }
+
+    IEnumerator _ChangeCanvas()
+    {
+        yield return new WaitForSeconds(3.5f);
+        panel1.SetActive(false);
+        panel2.SetActive(true);
     }
 }
