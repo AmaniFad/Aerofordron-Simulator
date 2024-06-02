a5b990bdc72d354b3ac4acdec23870a36a78ae36
AmaniFad <a22amafaddok@inspedralbes.cat>
2024-06-02 20:43:55 +0200

    finiquitao

--- Assets/Script/ModoCarrera/Poster.cs
+++ Assets/Script/ModoCarrera/Poster.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Poster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private UnityEvent onPasPosterMC;
+    [SerializeField] private UnityEvent onPasPosterP;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Timer.instance.GetModoCarrera())
+            {
+                onPasPosterMC.Invoke();
+            }
+            else
+            {
+                onPasPosterP.Invoke();
+            }
+        }
     }
 }
--- Assets/Script/ModoCarrera/Timer.cs
+++ Assets/Script/ModoCarrera/Timer.cs
@@ -8,34 +8,95 @@ public class Timer : MonoBehaviour
     [SerializeField] private TMP_Text timerText;
     [SerializeField] private float remainingTime;
 
+    [Header("Canva")]
+    [SerializeField] private GameObject canvaLose;
+    [SerializeField] private TMP_Text timerTextCanvaWin;
     private float elapsedTime;
-    private bool modoCarrera;
+    public bool modoCarrera;
+    public static Timer instance;
 
+    private float saveRemainingTime;
+    private bool startGame;
     public void SetModoCarrera(bool modoCarrera)
     {
         this.modoCarrera = modoCarrera;
     }
+    public bool GetModoCarrera()
+    {
+        return modoCarrera;
+    }
+    public void SetStartGame(bool startGame)
+    {
+        this.startGame = startGame;
+    }
     void Start()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
+        saveRemainingTime = remainingTime;
+        elapsedTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (modoCarrera)
+        if (startGame)
         {
-            remainingTime -= Time.deltaTime;
-            int min = Mathf.FloorToInt(remainingTime / 60);
-            int sec = Mathf.FloorToInt(remainingTime % 60);
-            timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+            if (modoCarrera)
+            {
+                remainingTime -= Time.deltaTime;
+                int min = Mathf.FloorToInt(remainingTime / 60);
+                int sec = Mathf.FloorToInt(remainingTime % 60);
+                timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+                elapsedTime += Time.deltaTime;
+
+                if (remainingTime < 14)
+                {
+                    timerText.color = Color.red;
+                }
+                if (elapsedTime <= 0)
+                {
+                    Lose();
+                }
+            }
+            else
+            {
+                elapsedTime += Time.deltaTime;
+                int min = Mathf.FloorToInt(elapsedTime / 60);
+                int sec = Mathf.FloorToInt(elapsedTime % 60);
+                timerText.text = string.Format("{0:00}:{1:00}", min, sec);
+            }
         }
-        else
-        {
-            elapsedTime += Time.deltaTime;
-            int min = Mathf.FloorToInt(elapsedTime / 60);
-            int sec = Mathf.FloorToInt(elapsedTime % 60);
-            timerText.text = string.Format("{0:00}:{1:00}", min, sec);
-        }
+    }
+    private void Lose()
+    {
+        canvaLose.SetActive(true);
+        StopTime();
+    }
+    public void RestartMC()
+    {
+        remainingTime = saveRemainingTime;
+    }
+    public void RestarP()
+    {
+        elapsedTime = 0;
+    }
+    public void RestartTime()
+    {
+        Time.timeScale = 1.0f; 
+        Cursor.visible = false;
+    }
+    public void StopTime()
+    {
+        Time.timeScale = 0.0f;
+        Cursor.visible = true;
+    }
+    public void setTextTime()
+    {
+        int min = Mathf.FloorToInt(elapsedTime / 60);
+        int sec = Mathf.FloorToInt(elapsedTime % 60);
+        timerTextCanvaWin.text = string.Format("{0:00}:{1:00}", min, sec);
     }
 }
