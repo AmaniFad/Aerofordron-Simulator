904735d4b67c986d09951306da1ac79c166de373
RemyGSP <guillem0405@gmail.com>
2025-01-30 11:46:32 +0100

    ds

--- Assets/Script/PlayFab/PlayFabLogin.cs
+++ Assets/Script/PlayFab/PlayFabLogin.cs
@@ -50,122 +50,30 @@ public class PlayFabLogin : MonoBehaviour
     }
     private void OnLoginSuccess(LoginResult result)
     {
-        PlayFabClientAPI.GetUserData(new GetUserDataRequest { Keys = new List<string> { "IsInizalized" } },
-           dataResult =>
-        {
-           if (dataResult.Data.ContainsKey("IsInizalized") == false)
-           {
-                InizalizeUser();
-           }
-           else
-           {
-                comprobeUserLogin();
-           }
-        }, error => {});
+        LoginWithPlayFabRequest request = new LoginWithPlayFabRequest();
+        request.Username = playerNameInput.text;
+        request.Password = playerPasswordInput.text;
+        request.TitleId = PlayFabSettings.staticSettings.TitleId;
+        PlayFabClientAPI.LoginWithPlayFab(request,LoginCallback,OnLoginFailure);
+        
     }
-    private void comprobeUserLogin()
+    private void LoginCallback(LoginResult result)
     {
-        PlayFabClientAPI.GetUserData(new GetUserDataRequest { Keys = new List<string> { "PlayerValues" } },
-           dataResult =>
-           {
-               if (dataResult.Data.ContainsKey("PlayerValues"))
-               {
-                   var playerValuesJson = dataResult.Data["PlayerValues"].Value;
-                   var playerValuesList = JsonUtility.FromJson<PlayerValues>(playerValuesJson);
-                   print(playerValuesList.UserName + " Pass: " + playerValuesList.Password);
-                   if (playerValuesList.UserName.Equals(playerName) && playerValuesList.Password.Equals(playerPassword))
-                   {
-                       enterSimulator.Invoke();
-                   }
-                   else
-                   {
-                       incorrectUserText.text = "Incorrect User or Password";
-                       playerNameInput.text = string.Empty;
-                       playerPasswordInput.text = string.Empty;
-                       StartCoroutine(DeleteText());
-                   }
-               }
-               else
-               {
-                   Debug.LogWarning("No se encontraron los datos del jugador.");
-                   incorrectUserText.text = "User data not found";
-                   StartCoroutine(DeleteText());
-               }
-
-           }, error => { });
+        enterSimulator.Invoke();
+        print(result);
     }
-    private void InizalizeUser()
-    {
-        GetTitleDataRequest request = new GetTitleDataRequest
-        {
-            Keys = new List<string>() { "InitialUsersList" }
-        };
-        PlayFabClientAPI.GetTitleData(request, dataResult =>
-        {
-            var data = dataResult.Data["InitialUsersList"];
-            var initialusersList = JsonUtility.FromJson<InitialUsersList>(data);
 
-            if (playerName != null && playerPassword != null)
-            {
-                isLoggedIn = false;
-                for (int i = 0; i < initialusersList.Users.Count; i++)
-                {
-                    if (initialusersList.Users[i].UserName.Equals(playerName)
-                    && initialusersList.Users[i].Password.Equals(playerPassword))
-                    {
-                        isLoggedIn = true;
-                    }
-                }
-                if (isLoggedIn)
-                {
-
-                    PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest
-                    {
-                        Data = new Dictionary<string, string>
-                        {
-                            {
-                                "IsInizalized", 
-                                JsonUtility.ToJson(new UserInizalized{isInizalized = true})
-                            },
-                            { 
-                                "PlayerValues",
-                                JsonUtility.ToJson(new PlayerValues{UserName = playerName, Password = playerPassword})
-                            },
-                        },
-
-                    }, result => { }, error => { });
-                    enterSimulator.Invoke();
-                }
-                else
-                {
-                    incorrectUserText.text = "Incorrect User or Password";
-                    playerNameInput.text = string.Empty;
-                    playerPasswordInput.text = string.Empty;
-                    StartCoroutine(DeleteText());
-                }
-            }
-        }, error => { });
-    }
     private void OnLoginFailure(PlayFabError error)
     {
+        incorrectUserText.text = "Incorrect User or Password";
+        playerNameInput.text = string.Empty;
+        playerPasswordInput.text = string.Empty;
+        StartCoroutine(DeleteText());
         Debug.LogWarning("Something went wrong with your first API call.  :(");
         Debug.LogError("Here's some debug information:");
         Debug.LogError(error.GenerateErrorReport());
     }
-    public void GetName()
-    {
-        if(playerNameInput != null)
-        {
-            playerName = playerNameInput.text;
-        }
-    }
-    public void GetPassword()
-    {
-        if (playerPasswordInput != null)
-        {
-            playerPassword = playerPasswordInput.text;
-        }
-    }
+
     public void PressedLoading()
     {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
