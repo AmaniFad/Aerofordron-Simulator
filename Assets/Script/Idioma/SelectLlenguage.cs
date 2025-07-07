9bf0c38fc22c2e6226a5e106ef8bbc2dbe7d1bae
AmaniFad <amani.fad12@gmail.com>
2025-07-07 17:27:39 +0100

    idioma implementado

--- Assets/Script/Idioma/SelectLlenguage.cs
+++ Assets/Script/Idioma/SelectLlenguage.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Localization.Settings;
 
 public class SelectLlenguage : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool active = false;
+    private void Start()
     {
-        
+        int ID = PlayerPrefs.GetInt("LocaleLenguaje", 0);
+        ChangeLocale(ID);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ChangeLocale(int localeID)
     {
+        if (active== true)
+          return;
         
+        StartCoroutine(SetLocale(localeID));
+    }
+    IEnumerator SetLocale(int _localeID)
+    {
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+        PlayerPrefs.SetInt("LocaleLenguaje", _localeID);
+        active = false;
     }
 }
