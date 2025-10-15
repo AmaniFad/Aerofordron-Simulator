608743c415f881864bb8de0028311dcfe56b534c
RemyGSP <guillem0405@gmail.com>
2025-10-15 11:28:27 +0200

    d

--- Assets/Script/ShowModeNameOnButtonHover.cs
+++ Assets/Script/ShowModeNameOnButtonHover.cs
@@ -15,14 +15,24 @@ public class ShowModeNameOnButtonHover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (EventSystem.current.currentSelectedGameObject == this.gameObject)
-        {
-            modeName.SetActive(true);
-        }
-        else
-        {
-            modeName.SetActive(false);
-        }
+        //if (EventSystem.current.currentSelectedGameObject == this.gameObject)
+        //{
+        //    modeName.SetActive(true);
+        //}
+        //else
+        //{
+        //    modeName.SetActive(false);
+        //}
 
+    }
+
+    public void ShowName()
+    {
+        modeName.SetActive(true);
+    }
+
+    public void HideName()
+    {
+        modeName.SetActive(false);
     }
 }
