80015e95196611651e3cc91c2ac17598b08cc04f
AmaniFad <amani.fad12@gmail.com>
2025-10-28 20:31:11 +0000

    scene regar arreglada (las ui se superponen)

--- Assets/Script/Interfaces/DronHud/CompassDron.cs
+++ Assets/Script/Interfaces/DronHud/CompassDron.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 
 public class CompassDron : MonoBehaviour
 {
-    [SerializeField] private Transform player;          // Cámara o jugador
-    [SerializeField] private Transform dron;          // Cámara o jugador
-    [SerializeField] private RawImage compassImage;     // RawImage con la textura de brújula
+    [SerializeField] private Transform player;          
+    [SerializeField] private Transform dron;
+    [SerializeField] private RawImage compassImage;
+    [SerializeField] private RectTransform playerMarker;
+    private float compassWidth;
 
     [SerializeField] private Sprite GreenBanner;
     [SerializeField] private Sprite YellowBanner;
@@ -16,7 +18,10 @@ public class CompassDron : MonoBehaviour
     private void Start()
     {
         player = PlayerReferences.instance.GetPlayer().transform;
-        dron = PlayerReferences.instance.GetDron().transform;  
+        dron = PlayerReferences.instance.GetDron().transform;
+
+        if(compassImage != null)
+            compassWidth = compassImage.rectTransform.rect.width;
     }
     void Update()
     {
@@ -40,6 +45,38 @@ public class CompassDron : MonoBehaviour
         {
             BannerObj.GetComponent<Image>().sprite = GreenBanner;
             BannerText.text = "GPS MODE";
+        }
+        UpdatePlayerMarker();
+    }
+    private void UpdatePlayerMarker()
+    {
+        if (playerMarker != null)
+        {
+            // Vector desde el dron hacia el player (plano XZ)
+            Vector3 toPlayer = player.position - dron.position;
+            toPlayer.y = 0f;
+
+            if (toPlayer.sqrMagnitude > 0.0001f)
+            {
+                // Ángulo absoluto del vector (0° = norte/+Z)
+                float bearingWorld = Mathf.Atan2(toPlayer.x, toPlayer.z) * Mathf.Rad2Deg;
+
+                // Ángulo relativo al frente del dron (-180..180)
+                float relativeDeg = Mathf.DeltaAngle(dron.eulerAngles.y, bearingWorld);
+
+                // Mapea -180..180 a -ancho/2..+ancho/2 (centro = frente del dron)
+                float halfWidth = compassImage.rectTransform.rect.width * 0.5f;
+                float x = (relativeDeg / 180f) * halfWidth;
+
+                // Si tu compás “va al revés”, invierte con x = -x;
+                playerMarker.anchoredPosition = new Vector2(x, playerMarker.anchoredPosition.y);
+                if (!playerMarker.gameObject.activeSelf) playerMarker.gameObject.SetActive(true);
+            }
+            else
+            {
+                // Opcional: ocultar si está exactamente encima
+                playerMarker.gameObject.SetActive(false);
+            }
         }
     }
 }
