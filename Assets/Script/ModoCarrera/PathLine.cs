bcbab9ba9c14405af6eb1c3e546a3413ac5a5a13
Amani Fadli <amani.fad12@gmail.com>
2026-04-28 18:02:36 +0200

    pathline

--- Assets/Script/ModoCarrera/PathLine.cs
+++ Assets/Script/ModoCarrera/PathLine.cs
@@ -23,13 +23,50 @@ public class PathLine : MonoBehaviour
         }
         if (waypoints != null && waypoints.Count > 0)
         {
-            lineRenderer.positionCount = waypoints.Count;
+            /*lineRenderer.positionCount = waypoints.Count;
             for (int i = 0; i < waypoints.Count; i++)
             {
                 lineRenderer.SetPosition(i, waypoints[i].position);
                 //waypoints[i].gameObject.GetComponent<BoxCollider>().enabled = true;
+            }*/
+            DrawSmoothLine();
+        }
+    }
+    void DrawSmoothLine()
+    {
+        List<Vector3> points = new List<Vector3>();
+        List<Vector3> wp = new List<Vector3>();
+
+        wp.Add(waypoints[0].position);
+
+        foreach (var w in waypoints)
+            wp.Add(w.position);
+
+        wp.Add(waypoints[waypoints.Count - 1].position);
+        for (int i = 0; i < wp.Count - 3; i++)
+        {
+            for (float t = 0; t <= 1; t += 0.1f)
+            {
+                points.Add(CatmullRom(
+                    wp[i],
+                    wp[i + 1],
+                    wp[i + 2],
+                    wp[i + 3],
+                    t
+                ));
             }
         }
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPositions(points.ToArray());
+    }
+    Vector3 CatmullRom(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        return 0.5f * (
+            (2 * p1) +
+            (-p0 + p2) * t +
+            (2 * p0 - 5 * p1 + 4 * p2 - p3) * t * t +
+            (-p0 + 3 * p1 - 3 * p2 + p3) * t * t * t
+        );
     }
 
     public void Restart()
