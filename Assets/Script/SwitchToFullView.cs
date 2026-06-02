0b18d240c3ec40e4eefb110df08f54411d765ef5
RemyGSP <guillem0405@gmail.com>
2026-06-02 11:54:27 +0200

    CorregidoErrorFullview

--- Assets/Script/SwitchToFullView.cs
+++ Assets/Script/SwitchToFullView.cs
@@ -7,35 +7,39 @@ public class SwitchToFullView : MonoBehaviour
     public static SwitchToFullView instance;
     [SerializeField] private GameObject fullViewCamera;
     [SerializeField] private GameObject hudPlayer;
-
+    private GameObject oldCamera;
+    private bool fullview;
     private void Start()
     {
         hudPlayer = PlayerReferences.instance.GetHUD();
         instance = this;
+        oldCamera = Camera.main.gameObject;
     }
     // Update is called once per frame
     void Update()
     {
-        if (PlayerInputController.Instance.IsFullView() && !PlayerStateController.instance.CanMove())
+        if (PlayerInputController.Instance.IsFullView() && !PlayerStateController.instance.CanMove() && !fullview)
         {
             EnterFullView();
+            fullview = true;
         }
-        else
+        else if (fullview && !PlayerInputController.Instance.IsFullView())
         {
+            fullview = false;
             ExitFullView();
         }
     }
 
     public void EnterFullView()
     {
-        Camera.main.gameObject.SetActive(false);
+        oldCamera.SetActive(false);
         fullViewCamera.SetActive(true);
         hudPlayer.SetActive(false);
     }
 
     public void ExitFullView()
     {
-        Camera.main.gameObject.SetActive(true);
+        oldCamera.SetActive(true);
         fullViewCamera.SetActive(false);
         hudPlayer.SetActive(true);
     }
